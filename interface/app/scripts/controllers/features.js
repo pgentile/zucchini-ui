0b600f7ab0dfa25a0f05a643c97a4c454e2a9f21
Pierre Gentile <pierre.gentile.perso@gmail.com>
2015-12-13 17:33:21 +0100

    Directive pour les tags

--- interface/app/scripts/controllers/features.js
+++ interface/app/scripts/controllers/features.js
@@ -1,6 +1,6 @@
 'use strict';
 
-(function (angular, document) {
+(function (angular) {
 
   var FeatureLoader = function (AllFeaturesResource) {
 
@@ -53,83 +53,7 @@
           controller: 'FeatureCtrl',
           controllerAs: 'ctrl'
         });
-    })
-    .directive('tcStatus', function () {
-      return {
-        restrict: 'E',
-        scope: {
-          status: '=status'
-        },
-        templateUrl: 'views/tc-status.html'
-      };
-    })
-    .directive('tcElementInfo', function () {
-
-      var cut = function (info) {
-        var name = info.name;
-        var previousOffset = 0;
-
-        var items = [];
-
-        info.arguments.forEach(function (arg) {
-          // Ajouter le contenu avant l'argument
-          var before = name.substring(previousOffset, arg.offset);
-          if (before.length > 0) {
-            items.push({ type: 'text', value: before });
-          }
-
-          // Extraire la valeur de l'argument
-          items.push({ type: 'arg', value: arg.value });
-
-          previousOffset = arg.offset + arg.value.length;
-        });
-
-        // Ajouter le contenu restant
-        var remaining = name.substring(previousOffset);
-        if (remaining.length > 0) {
-          items.push({ type: 'text', value: remaining });
-        }
-
-        return items;
-      };
-
-      var link = function (scope, element) {
-
-        scope.$watch('info', function (info) {
-          element.empty();
-
-          // Keyword
-          var keywordElem = angular.element('<b></b>');
-          keywordElem.text(info.keyword);
-          element.append(keywordElem);
-
-          element.append(' ');
-
-          // Name
-          cut(info).forEach(function (part) {
-            var newElement;
-            if (part.type === 'arg') {
-              newElement = angular.element('<code></code>');
-              newElement.text(part.value);
-            } else {
-              newElement = document.createTextNode(part.value);
-            }
-            element.append(newElement);
-          });
-
-        });
-
-      };
-
-      return {
-        restrict: 'E',
-        scope: {
-          info: '=info'
-        },
-        link: link
-      };
-
     });
 
 
-})(angular, window.document);
+})(angular);
