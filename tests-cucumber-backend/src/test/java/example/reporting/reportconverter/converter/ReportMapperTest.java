b5eda76d8779c90761dd7a6509e67f478f76fdf4
Pierre Gentile <pierre.gentile.perso@gmail.com>
2016-01-10 22:24:58 +0100

    Unit test for report mapper

---
+++ tests-cucumber-backend/src/test/java/example/reporting/reportconverter/converter/ReportMapperTest.java
@@ -0,0 +1,66 @@
+package example.reporting.reportconverter.converter;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionLikeType;
+import com.google.common.io.Resources;
+import example.reporting.feature.domain.Feature;
+import example.reporting.feature.domain.FeatureFactory;
+import example.reporting.reportconverter.report.ReportFeature;
+import example.reporting.scenario.domain.Scenario;
+import example.reporting.scenario.domain.ScenarioFactory;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.io.InputStream;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.anyString;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ReportMapperTest {
+
+    @Mock
+    private FeatureFactory featureFactory;
+
+    @Mock
+    private ScenarioFactory scenarioFactory;
+
+    private ReportMapper reportMapper;
+
+    @Before
+    public void setUp() throws Exception {
+        reportMapper = new ReportMapper(featureFactory, scenarioFactory);
+        reportMapper.initMapper();
+    }
+
+    @Test
+    public void should_parse_report() throws Exception {
+        // given
+        final ObjectMapper objectMapper = new ObjectMapper()
+            .findAndRegisterModules()
+            .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+
+        final CollectionLikeType reportFeatureListType = objectMapper.getTypeFactory().constructCollectionType(List.class, ReportFeature.class);
+
+        final List<ReportFeature> reportFeatures;
+        try (InputStream stream = Resources.asByteSource(Resources.getResource("report.json")).openStream()) {
+            reportFeatures = objectMapper.readValue(stream, reportFeatureListType);
+        }
+
+        given(featureFactory.create(anyString())).willAnswer(args -> new Feature());
+        given(scenarioFactory.create(anyString(), anyString())).willAnswer(args -> new Scenario());
+
+        // when
+        final List<Feature> features = reportMapper.mapAsList(reportFeatures, Feature.class);
+
+        // then
+        assertThat(features).hasSameSizeAs(reportFeatures);
+    }
+
+}
