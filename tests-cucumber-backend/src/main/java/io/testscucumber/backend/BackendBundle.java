05d0a3be769f76fae11635e2742f1057fdbb86ff
Pierre Gentile <pierre.gentile.perso@gmail.com>
2016-01-17 00:06:01 +0100

    Don't serialize null fields

--- tests-cucumber-backend/src/main/java/io/testscucumber/backend/BackendBundle.java
+++ tests-cucumber-backend/src/main/java/io/testscucumber/backend/BackendBundle.java
@@ -1,5 +1,6 @@
 package io.testscucumber.backend;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -25,7 +26,8 @@ public class BackendBundle implements ConfiguredBundle<BackendConfiguration> {
 
         bootstrap.getObjectMapper()
             .registerModules(new JavaTimeModule(), new Jdk8Module())
-            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .setSerializationInclusion(JsonInclude.Include.NON_NULL);
     }
 
     @Override
