538c5b897f27c2a65662bbdfb5cc5f5a9763b1cb
Pierre Gentile <pierre.gentile.perso@gmail.com>
2016-01-16 15:00:41 +0100

    Code quality

--- tests-cucumber-capsule/src/main/java/io/testscucumber/capsule/TestsCucumberConfiguration.java
+++ tests-cucumber-capsule/src/main/java/io/testscucumber/capsule/TestsCucumberConfiguration.java
@@ -1,6 +1,5 @@
 package io.testscucumber.capsule;
 
-import io.dropwizard.server.AbstractServerFactory;
 import io.testscucumber.backend.BackendConfiguration;
 
 public class TestsCucumberConfiguration extends BackendConfiguration {
