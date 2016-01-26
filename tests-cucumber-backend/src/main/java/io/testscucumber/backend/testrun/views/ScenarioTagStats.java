83a518617ac631376868cefb75abc1d3229578ab
Pierre Gentile <pierre.gentile.perso@gmail.com>
2016-01-26 21:49:01 +0100

    Status for tag

--- tests-cucumber-backend/src/main/java/io/testscucumber/backend/testrun/views/ScenarioTagStats.java
+++ tests-cucumber-backend/src/main/java/io/testscucumber/backend/testrun/views/ScenarioTagStats.java
@@ -2,6 +2,8 @@ package io.testscucumber.backend.testrun.views;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.testscucumber.backend.feature.domain.FeatureStatus;
+import io.testscucumber.backend.scenario.domain.ScenarioStatus;
 import io.testscucumber.backend.scenario.views.ScenarioStats;
 
 public class ScenarioTagStats {
@@ -22,6 +24,22 @@ public class ScenarioTagStats {
 
     public ScenarioStats getStats() {
         return stats;
+    }
+
+    public FeatureStatus getStatus() {
+        if (stats.getCount() == 0) {
+            return FeatureStatus.NOT_RUN;
+        }
+        if (stats.getStatsByStatus().get(ScenarioStatus.FAILED) > 0) {
+            return FeatureStatus.FAILED;
+        }
+        if (stats.getStatsByStatus().get(ScenarioStatus.PASSED) == stats.getCount()) {
+            return FeatureStatus.PASSED;
+        }
+        if (stats.getStatsByStatus().get(ScenarioStatus.NOT_RUN) == stats.getCount()) {
+            return FeatureStatus.NOT_RUN;
+        }
+        return FeatureStatus.PARTIAL;
     }
 
 }
