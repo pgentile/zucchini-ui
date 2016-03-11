11f12bfbd8b600d245f5ba8c27af3515d78ec1ad
Pierre Gentile <pierre.gentile.perso@gmail.com>
2016-03-11 20:18:08 +0100

    Remove useless code

--- tests-cucumber-backend/src/main/java/io/testscucumber/backend/support/ddd/morphia/MorphiaRepository.java
+++ tests-cucumber-backend/src/main/java/io/testscucumber/backend/support/ddd/morphia/MorphiaRepository.java
@@ -1,18 +1,13 @@
 package io.testscucumber.backend.support.ddd.morphia;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBObject;
 import io.testscucumber.backend.support.ddd.ConcurrentEntityModificationException;
 import io.testscucumber.backend.support.ddd.EntityNotFoundException;
-import io.testscucumber.backend.support.ddd.RefreshableEntityRepository;
 import io.testscucumber.backend.support.ddd.Repository;
 import org.mongodb.morphia.dao.BasicDAO;
-import org.mongodb.morphia.mapping.Mapper;
-import org.mongodb.morphia.mapping.cache.EntityCache;
 
 import java.util.ConcurrentModificationException;
 
-public class MorphiaRepository<T, I> implements Repository<T, I>, RefreshableEntityRepository<T, I> {
+public class MorphiaRepository<T, I> implements Repository<T, I> {
 
     private final BasicDAO<T, I> dao;
 
@@ -41,21 +36,6 @@ public class MorphiaRepository<T, I> implements Repository<T, I>, RefreshableEnt
     @Override
     public void delete(final T entity) {
         dao.delete(entity);
-    }
-
-    @Override
-    public void refresh(final T entity) {
-        final Mapper mapper = dao.getDs().getMapper();
-
-        final Object id = mapper.getKey(entity).getId();
-        final DBObject query = new BasicDBObject(Mapper.ID_KEY, id);
-        final DBObject dbObj = dao.getCollection().findOne(query);
-        if (dbObj == null) {
-            throw new EntityNotFoundException(dao.getEntityClass(), "Not found for ID " + id);
-        }
-
-        final EntityCache entityCache = mapper.createEntityCache();
-        mapper.fromDb(dao.getDatastore(), dbObj, entity, entityCache);
     }
 
 }
