b515ed4ce5b97a3f9a3fa836c7414e7be8ca8002
Pierre Gentile <pgentile@voyages-sncf.com>
2016-03-21 14:00:13 +0100

    Update reviewed state for non passed scenarii, otherwise, stats can't be calculated

---
+++ tests-cucumber-mongo/migrations/0007-non-successful-scenarii-are-not-reviewed.js
@@ -0,0 +1,16 @@
+migrate(function () {
+
+  db.scenarii.update(
+    {
+      status: { $not: { $eq: 'PASSED' } },
+      reviewed: { $exists: false }
+    },
+    {
+      $set: { reviewed: false }
+    },
+    {
+      multi: true
+    }
+  );
+
+});
