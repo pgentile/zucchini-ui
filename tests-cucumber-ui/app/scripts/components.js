812a13135c6404932d0dfbd2861026e37490590d
Pierre Gentile <pierre.gentile.perso@gmail.com>
2016-06-15 23:46:04 +0200

    Pie chart as a component instead of a directive

--- tests-cucumber-ui/app/scripts/components.js
+++ tests-cucumber-ui/app/scripts/components.js
@@ -286,21 +286,28 @@
         this.$postLink = function () {
           // Attach chart to current element
           this.chart = new Chartist.Pie($element[0]);
+          this.updateChart();
         };
 
         this.$onChanges = function () {
           // Init selected features to bound features
-          if (this.data && _.isNumber(this.total)) {
-            this.chart.update(this.data, {
-              total: this.total,
-              donut: true,
-            }, true);
+          if (this.chart && this.data && _.isNumber(this.total)) {
+            this.updateChart();
           }
         };
 
         this.$onDestroy = function () {
           // Release chart resources on directive destroy
           this.chart.detach();
+        };
+
+        this.updateChart = function () {
+          if (this.chart) {
+            this.chart.update(this.data, {
+              total: this.total,
+              donut: true,
+            }, true);
+          }
         };
 
     }
