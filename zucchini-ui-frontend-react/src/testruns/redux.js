97cc35d80685706c32e09dac534cc8f3a8d0f143
Pierre Gentile <pierre.gentile.perso@gmail.com>
2017-03-11 10:46:29 +0100

    Load stats after test run load

--- zucchini-ui-frontend-react/src/testruns/redux.js
+++ zucchini-ui-frontend-react/src/testruns/redux.js
@@ -7,14 +7,54 @@ const PREFIX = 'TEST_RUNS';
 
 const GET_LATEST_TEST_RUNS = `${PREFIX}/GET_LATEST_TEST_RUNS`;
 const GET_LATEST_TEST_RUNS_FULFILLED = `${GET_LATEST_TEST_RUNS}_FULFILLED`;
-// const GET_LATEST_TEST_RUNS_REJECTED = `$${GET_LATEST_TEST_RUNS}_REJECTED`;
+
+const GET_LATEST_TEST_RUNS_WITH_STATS = `${PREFIX}/GET_LATEST_TEST_RUNS_WITH_STATS`;
+const GET_LATEST_TEST_RUNS_WITH_STATS_FULFILLED = `${GET_LATEST_TEST_RUNS_WITH_STATS}_FULFILLED`;
 
 
 export function getLatestTestRuns() {
-  return {
-    type: GET_LATEST_TEST_RUNS,
-    payload: testRunApi.getLatests({ withStats: true }),
+  return dispatch => {
+    const latestTestRuns = dispatch({
+      type: GET_LATEST_TEST_RUNS,
+      payload: getLatestsTestRuns(),
+    });
+
+    latestTestRuns.then(() => {
+      dispatch({
+        type: GET_LATEST_TEST_RUNS_WITH_STATS,
+        payload: getLatestsTestRunsWithStats(),
+      });
+    });
   };
+}
+
+
+function getLatestsTestRuns() {
+  // Fake stats
+  const defaultStats = {
+    count: null,
+    passed: null,
+    failed: null,
+    pending: null,
+    notRun: null,
+  };
+
+  return testRunApi.getLatests({ withStats: false })
+    .then(testRuns => {
+      return testRuns.map(testRun => ({
+        ...testRun,
+        stats: {
+          all: defaultStats,
+          reviewed: defaultStats,
+          nonReviewed: defaultStats,
+        },
+      }))
+    });
+}
+
+
+function getLatestsTestRunsWithStats() {
+  return testRunApi.getLatests({ withStats: true });
 }
 
 
@@ -37,6 +77,7 @@ export function testRuns(state = initialState, action) {
     };
 
   case GET_LATEST_TEST_RUNS_FULFILLED:
+  case GET_LATEST_TEST_RUNS_WITH_STATS_FULFILLED:
     return {
       ...state,
       testRuns: action.payload,
