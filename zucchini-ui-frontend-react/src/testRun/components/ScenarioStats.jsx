637822c42a859a3062e788429badcaeb3e8ad317
Pierre Gentile <pierre.gentile.perso@gmail.com>
2017-03-12 10:18:39 +0100

    Legend for stats

--- zucchini-ui-frontend-react/src/testRun/components/ScenarioStats.jsx
+++ zucchini-ui-frontend-react/src/testRun/components/ScenarioStats.jsx
@@ -4,6 +4,7 @@ import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 
 import ScenarioPieChart from './StatsPieChart';
+import StatsLegend from './StatsLegend';
 
 
 export default class ScenarioStats extends React.Component {
@@ -15,16 +16,36 @@ export default class ScenarioStats extends React.Component {
       <Grid>
         <Row>
           <Col md={4}>
-            <h5 className="text-center">Tous les scénarii</h5>
+            <h5 className="text-center"><b>Tous les scénarii</b></h5>
+          </Col>
+          <Col md={4}>
+            <h5 className="text-center"><b>Scénarii analysés</b></h5>
+          </Col>
+          <Col md={4}>
+            <h5 className="text-center"><b>Scénarii non analysés</b></h5>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={4}>
             <ScenarioPieChart stats={stats.all} total={stats.all.count} />
           </Col>
           <Col md={4}>
-            <h5 className="text-center">Scénarii analysés</h5>
             <ScenarioPieChart stats={stats.reviewed} total={stats.all.count} />
           </Col>
           <Col md={4}>
-            <h5 className="text-center">Scénarii non analysés</h5>
             <ScenarioPieChart stats={stats.nonReviewed} total={stats.all.count} />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={4}>
+            <StatsLegend stats={stats.all} />
+          </Col>
+          <Col md={4}>
+            <StatsLegend stats={stats.reviewed} />
+
+          </Col>
+          <Col md={4}>
+            <StatsLegend stats={stats.nonReviewed} />
           </Col>
         </Row>
       </Grid>
---
+++ zucchini-ui-frontend-react/src/testRun/components/StatsLegend.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import Table from 'react-bootstrap/lib/Table';
+import Label from 'react-bootstrap/lib/Label';
+
+
+export default class StatsLegend extends React.PureComponent {
+
+  render() {
+    const { stats } = this.props;
+
+    return (
+      <Table condensed>
+        <thead>
+          <tr>
+            <th>&nbsp;</th>
+            <th>Statut</th>
+            <th>Nombre</th>
+            <th>%</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><Label bsStyle="success">&nbsp;</Label></td>
+            <td>Succès</td>
+            <td>{stats.passed}</td>
+            <td>{numberAsPercent(100 * stats.passed / stats.count)}</td>
+          </tr>
+          <tr>
+            <td><Label bsStyle="warning">&nbsp;</Label></td>
+            <td>En attente</td>
+            <td>{stats.pending}</td>
+            <td>{numberAsPercent(100 * stats.pending / stats.count)}</td>
+          </tr>
+          <tr>
+            <td><Label bsStyle="danger">&nbsp;</Label></td>
+            <td>Échecs</td>
+            <td>{stats.failed}</td>
+            <td>{numberAsPercent(100 * stats.failed / stats.count)}</td>
+          </tr>
+          <tr>
+            <td><Label bsStyle="default">&nbsp;</Label></td>
+            <td>Non joués</td>
+            <td>{stats.notRun}</td>
+            <td>{numberAsPercent(100 * stats.notRun / stats.count)}</td>
+          </tr>
+          <tr className="stats-legend-total">
+            <td>&nbsp;</td>
+            <td>Total</td>
+            <td>{stats.count}</td>
+            <td>{numberAsPercent(100 * stats.count / stats.count)}</td>
+          </tr>
+        </tbody>
+      </Table>
+    );
+  }
+
+}
+
+StatsLegend.propTypes = {
+  stats: React.PropTypes.object.isRequired,
+};
+
+
+function numberAsPercent(n) {
+  // Number formatting returned a result, this is a valid number
+  const formatted = n.toFixed(1);
+  if (formatted) {
+    return `${formatted} %`;
+  }
+  return 'N/A';
+}
