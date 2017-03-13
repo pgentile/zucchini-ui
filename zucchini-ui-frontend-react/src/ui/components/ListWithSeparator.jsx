32ba04cab570f83a76d9a368564787cdde6c12c1
Pierre Gentile <pierre.gentile.perso@gmail.com>
2017-03-14 00:40:54 +0100

    Comments

--- zucchini-ui-frontend-react/src/ui/components/ListWithSeparator.jsx
+++ zucchini-ui-frontend-react/src/ui/components/ListWithSeparator.jsx
@@ -6,6 +6,8 @@ export default function ListWithSeparator({ children, separator }) {
 
   if (items.length > 0) {
     const begining = items.slice(0, items.length - 1);
+    
+    // Remove last separator
     let last = items[items.length - 1];
     last = last.slice(0, 0);
 
