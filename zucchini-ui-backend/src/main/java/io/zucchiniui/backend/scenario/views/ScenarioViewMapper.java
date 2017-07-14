100f2676e7063f1cd07f5f2e89bd176c88760892
Pierre Gentile <pierre.gentile.perso@gmail.com>
2017-07-14 08:08:37 +0200

    Fix error message mapping

--- zucchini-ui-backend/src/main/java/io/zucchiniui/backend/scenario/views/ScenarioViewMapper.java
+++ zucchini-ui-backend/src/main/java/io/zucchiniui/backend/scenario/views/ScenarioViewMapper.java
@@ -2,12 +2,19 @@ package io.zucchiniui.backend.scenario.views;
 
 import io.zucchiniui.backend.scenario.domain.Scenario;
 import io.zucchiniui.backend.shared.views.AbstractConfigurableMapper;
+import ma.glasnost.orika.CustomConverter;
 import ma.glasnost.orika.MapperFactory;
+import ma.glasnost.orika.MappingContext;
+import ma.glasnost.orika.metadata.Type;
+
+import java.util.Optional;
 
 class ScenarioViewMapper extends AbstractConfigurableMapper {
 
     @Override
     protected void doConfigure(final MapperFactory factory) {
+        factory.getConverterFactory().registerConverter("optionalOrNull", new OptionalOrNullConverter<>());
+
         factory.classMap(Scenario.class, ScenarioListItemView.class)
             .byDefault()
             .register();
@@ -15,6 +22,20 @@ class ScenarioViewMapper extends AbstractConfigurableMapper {
         factory.classMap(Scenario.class, ScenarioHistoryItemView.class)
             .byDefault()
             .register();
+
+        factory.classMap(Scenario.class, FailedScenarioListItemView.class)
+            .fieldMap("errorMessage").aToB().converter("optionalOrNull").add()
+            .byDefault()
+            .register();
+    }
+
+    private static class OptionalOrNullConverter<T> extends CustomConverter<Optional<T>, T> {
+
+        @Override
+        public T convert(Optional<T> source, Type<? extends T> destinationType, MappingContext mappingContext) {
+            return source.orElse(null);
+        }
+
     }
 
 }
