32dcd5716a59b38b9944f5cae2991a398c069b70
Leconardel Jeremy <jeremy_leconardel@DX-8CN73300M4.groupevsc.com>
2017-10-05 09:49:36 +0200

    Add total on report

--- zucchini-ui-frontend/src/reports/components/ReportsPageContainer.js
+++ zucchini-ui-frontend/src/reports/components/ReportsPageContainer.js
@@ -10,7 +10,13 @@ const selectGroups = createSelector(
   features => {
     const groupSet = new Set(features.map(feature => feature.group));
     const groups = [];
-    let totalFailed = 0;
+    const allTotal = {
+      count: 0,
+      passed: 0,
+      failed: 0,
+      pending: 0,
+      notRun: 0
+    };
     for (const tmpGroup of groupSet.values()) {
       groups.push(
         {
@@ -18,7 +24,11 @@ const selectGroups = createSelector(
           stats: features.filter(feature => feature.group === tmpGroup)
             .map(feature => feature.stats)
             .reduce((aggregateStat, stat) => {
-              totalFailed += stat.all.failed;
+              allTotal.count = allTotal.count + stat.all.count;
+              allTotal.passed = allTotal.passed + stat.all.passed;
+              allTotal.failed = allTotal.failed + stat.all.failed;
+              allTotal.pending = allTotal.pending + stat.all.pending;
+              allTotal.notRun = allTotal.notRun + stat.all.notRun;
               return {
                 all: {
                   count: aggregateStat.all.count + stat.all.count,
@@ -32,11 +42,22 @@ const selectGroups = createSelector(
         })
     }
 
+
+    // Compute total
+    const total = {
+      id: 'total',
+      name: 'Total',
+      stats: {
+        all: allTotal
+      }
+    }
+    groups.push(total);
+
+    // Compute ratio
     for (const group of groups) {
       group.stats.all.faultRate = ((group.stats.all.failed / group.stats.all.count) * 100).toFixed(2);
-      group.stats.all.faultRateRepartition = ((group.stats.all.failed / totalFailed) * 100).toFixed(2);
+      group.stats.all.faultRateRepartition = ((group.stats.all.failed / total.stats.all.failed) * 100).toFixed(2);
     }
-
 
     return groups;
   }
