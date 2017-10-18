2e89e9ffa8cf272fd81cdbce451ac2662a80b325
Pierre Gentile <pierre.gentile.perso@gmail.com>
2017-10-18 20:14:54 +0200

    Upgraded to React 16
    + Upgraded all dependencies

--- zucchini-ui-frontend/test/helpers.js
+++ zucchini-ui-frontend/test/helpers.js
@@ -1,5 +1,5 @@
 import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15';
+import Adapter from 'enzyme-adapter-react-16';
 import chai from 'chai';
 import chaiEnzyme from 'chai-enzyme'
 
