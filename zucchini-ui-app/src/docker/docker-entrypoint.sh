a3576563ed9b5318df7f5bfc2eb4e9a7a2e8709a
Pierre Gentile <pierre.gentile.perso@gmail.com>
2018-11-25 16:03:24 +0100

    Docker image size optimization

--- zucchini-ui-app/src/docker/docker-entrypoint.sh
+++ zucchini-ui-app/src/docker/docker-entrypoint.sh
@@ -2,4 +2,10 @@
 
 set -e
 
-exec java ${JAVA_OPTS} -XX:-UseContainerSupport -showversion -jar /zucchini-ui/zucchini-ui-app-${ZUCCHINI_VERSION}-all.jar "$@"
+# Need more args ? Use the JDK_JAVA_OPTIONS option
+exec java \
+  -showversion \
+  -XX:-UseContainerSupport \
+  -Djava.awt.headless=true \
+  -jar /zucchini-ui/zucchini-ui-app-${ZUCCHINI_VERSION}-all.jar \
+  "$@"
