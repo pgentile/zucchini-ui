bb039e7924a8b87b75b5896029eb4475714c65bb
Pierre Gentile <pierre.gentile.perso@gmail.com>
2019-05-08 11:44:41 +0200

    Fixed error on production build

--- zucchini-ui-frontend/src/ui/components/ButtonLink.jsx
+++ zucchini-ui-frontend/src/ui/components/ButtonLink.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Button from "./Button";
 import { Link } from "react-router-dom";
 
 export default class ButtonLink extends React.PureComponent {
   static propTypes = {
-    to: Link.propTypes.to,
+    to: PropTypes.oneOfType([PropTypes.string, PropTypes.object]).isRequired,
     ...Button.propTypes
   };
 
