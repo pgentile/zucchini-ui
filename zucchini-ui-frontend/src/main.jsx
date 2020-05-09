0b84a655a7f2c5c84ea573e0a21868538ddf2d55
Pierre Gentile <pierre.gentile.perso@gmail.com>
2020-05-09 17:36:43 +0200

    Dependencis for Bootstrap 4 migration
    * Sass utilities
    * Imported bootstrap 4

--- zucchini-ui-frontend/src/main.jsx
+++ zucchini-ui-frontend/src/main.jsx
@@ -1,10 +1,11 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 
-import "./styles/main.less";
-
+import React from "react";
 import ReactDOM from "react-dom";
 
 import AppRouter from "./AppRouter";
 
-ReactDOM.render(AppRouter(), document.getElementById("content"));
+import "./main.scss";
+
+ReactDOM.render(<AppRouter />, document.getElementById("content"));
