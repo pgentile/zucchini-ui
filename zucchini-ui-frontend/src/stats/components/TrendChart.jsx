0b55acd207c85162e63439451c222b7cdb21ceed
Pierre Gentile <pierre.gentile.perso@gmail.com>
2020-05-10 21:37:26 +0200

    Colors for trend chart

--- zucchini-ui-frontend/src/stats/components/TrendChart.jsx
+++ zucchini-ui-frontend/src/stats/components/TrendChart.jsx
@@ -1,36 +1,36 @@
 import PropTypes from "prop-types";
-import React from "react";
+import React, { memo } from "react";
 import kebabCase from "lodash/kebabCase";
 
 import BarChart from "../../charts/components/BarChart";
 
-export default class TrendChart extends React.PureComponent {
-  static propTypes = {
-    trends: PropTypes.arrayOf(
-      PropTypes.shape({
-        passed: PropTypes.number.isRequired,
-        failed: PropTypes.number.isRequired,
-        pending: PropTypes.number.isRequired,
-        notRun: PropTypes.number.isRequired
-      })
-    ).isRequired
-  };
+import "./TrendChart.scss";
 
-  render() {
-    const { trends } = this.props;
-
-    const series = ["passed", "failed", "pending", "notRun"].map((type) => {
-      const serieValues = trends.map((trend) => {
-        return trend[type];
-      });
-
-      return {
-        name: type,
-        value: serieValues,
-        className: `chart-trend-${kebabCase(type)}`
-      };
+function TrendChart({ trends }) {
+  const series = ["passed", "failed", "pending", "notRun"].map((type) => {
+    const serieValues = trends.map((trend) => {
+      return trend[type];
     });
 
-    return <BarChart data={{ series }} />;
-  }
+    return {
+      name: type,
+      value: serieValues,
+      className: `chart-trend-${kebabCase(type)}`
+    };
+  });
+
+  return <BarChart data={{ series }} />;
 }
+
+TrendChart.propTypes = {
+  trends: PropTypes.arrayOf(
+    PropTypes.shape({
+      passed: PropTypes.number.isRequired,
+      failed: PropTypes.number.isRequired,
+      pending: PropTypes.number.isRequired,
+      notRun: PropTypes.number.isRequired
+    })
+  ).isRequired
+};
+
+export default memo(TrendChart);
