1e59659d9eaf6e20ce075781f0ba638b9f3351bc
Pierre Gentile <pierre.gentile.perso@gmail.com>
2020-05-10 11:40:59 +0200

    Bootstrap 4 : fixed navbar

--- zucchini-ui-frontend/src/ui/components/RootPage.jsx
+++ zucchini-ui-frontend/src/ui/components/RootPage.jsx
@@ -2,47 +2,37 @@ import PropTypes from "prop-types";
 import React, { Fragment } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
 import { Link } from "react-router-dom";
 
 import LoadingIndicator from "../../loadingIndicator/components/LoadingIndicator";
 import ErrorAlertContainer from "../../errors/components/ErrorAlertContainer";
 
-export default function RootPage({ search, children }) {
+export default function RootPage({ children }) {
   return (
     <Fragment>
       <LoadingIndicator />
 
-      <Navbar inverse staticTop collapseOnSelect>
-        <Navbar.Brand>
-          <Link to="/">Zucchini UI</Link>
+      <Navbar bg="dark" variant="dark" className="mb-4">
+        <Navbar.Brand as={Link} to="/">
+          Zucchini UI
         </Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse>
-          <Nav>
-            <NavItem>
-              <Link to="/">Derniers tirs</Link>
-            </NavItem>
-          </Nav>
-          {search}
-        </Navbar.Collapse>
+        <Nav className="mr-auto">
+          <Nav.Item>
+            <Nav.Link as={Link} to="/">
+              Derniers tirs
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
       </Navbar>
-      <div className="container">
+      <Container>
         <ErrorAlertContainer />
         {children}
-      </div>
+      </Container>
     </Fragment>
   );
 }
 
 RootPage.propTypes = {
-  children: PropTypes.node,
-  search: PropTypes.node
-};
-
-function NavItem({ children }) {
-  return <li>{children}</li>;
-}
-
-NavItem.propTypes = {
   children: PropTypes.node
 };
