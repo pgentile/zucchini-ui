9bd6f473aeb7e5be655ae464aadc85d5cdd257eb
Pierre Gentile <pierre.gentile.perso@gmail.com>
2020-05-16 12:19:07 +0200

    Removing tokens on output

--- zucchini-ui-frontend/src/ui/components/SimpleText.jsx
+++ zucchini-ui-frontend/src/ui/components/SimpleText.jsx
@@ -1,39 +1,38 @@
 import PropTypes from "prop-types";
-import React from "react";
+import React, { memo, useMemo } from "react";
 
 import tokenizeUrls from "../tokenizeUrls";
 import html from "../html";
 
-export default class SimpleText extends React.PureComponent {
-  static propTypes = {
-    text: PropTypes.string
-  };
-
-  render() {
-    const { text, ...otherProps } = this.props;
-
+function SimpleText({ text, ...otherProps }) {
+  const rawHtml = useMemo(() => {
+    let raw = "";
     if (text) {
       const tokens = tokenizeUrls(text);
-      let output = "";
       tokens.forEach(([type, value]) => {
         switch (type) {
           case "text":
-            output += html` ${value} `;
+            raw += html` ${value} `;
             break;
           case "url":
-            output += html` <a href="${value}">${value}</a> `;
+            raw += html` <a href="${value}">${value}</a> `;
             break;
           case "eol":
-            output += "<br/>";
+            raw += "<br/>";
             break;
           default:
             break;
         }
       });
-
-      return <p dangerouslySetInnerHTML={{ __html: output }} {...otherProps} tokens={tokens} />;
     }
+    return raw;
+  }, [text]);
 
-    return null;
-  }
+  return text ? <p dangerouslySetInnerHTML={{ __html: rawHtml }} {...otherProps} /> : null;
 }
+
+SimpleText.propTypes = {
+  text: PropTypes.string
+};
+
+export default memo(SimpleText);
