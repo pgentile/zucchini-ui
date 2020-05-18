d60e9cd1609745cf96e838417d19dcb055776de6
Pierre Gentile <pierre.gentile.perso@gmail.com>
2020-05-18 22:46:17 +0200

    Better looking button

--- zucchini-ui-frontend/src/ui/components/Button.jsx
+++ zucchini-ui-frontend/src/ui/components/Button.jsx
@@ -3,18 +3,16 @@ import React from "react";
 import { default as BootstrapButton } from "react-bootstrap/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default class Button extends React.PureComponent {
-  static propTypes = {
-    ...BootstrapButton.propTypes,
-    icon: PropTypes.string
-  };
-
-  render() {
-    const { icon, children, ...otherProps } = this.props;
-    return (
-      <BootstrapButton {...otherProps}>
-        {icon && <FontAwesomeIcon icon={icon} />} {children}
-      </BootstrapButton>
-    );
-  }
+export default function Button({ icon, children, ...otherProps }) {
+  return (
+    <BootstrapButton {...otherProps}>
+      {icon && <FontAwesomeIcon icon={icon} className="mr-2" />}
+      {children}
+    </BootstrapButton>
+  );
 }
+
+Button.propTypes = {
+  ...BootstrapButton.propTypes,
+  icon: PropTypes.object
+};
