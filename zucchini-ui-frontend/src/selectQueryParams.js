aa4fe3e78c2ab80077fe12283eae4f04bdd123d2
Pierre Gentile <pierre.gentile.perso@gmail.com>
2020-05-27 16:53:26 +0200

    Removed useless module

--- zucchini-ui-frontend/src/selectQueryParams.js
+++
@@ -1,7 +0,0 @@
-import { createSelector } from "reselect";
-import queryString from "query-string";
-
-export default createSelector(
-  (location) => location.search,
-  (search) => queryString.parse(search)
-);
