5a1704d04523bbfd02477fcf5a48207baf725f2a
Pierre Gentile <pierre.gentile.perso@gmail.com>
2020-05-28 13:57:36 +0200

    Better page loading indicator

--- zucchini-ui-frontend/src/loadingIndicator/components/PageLoadingPlaceholder.jsx
+++ zucchini-ui-frontend/src/loadingIndicator/components/PageLoadingPlaceholder.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { load, unload } from "../redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
 
 export default function PageLoadingPlaceholder() {
   const dispatch = useDispatch();
@@ -11,5 +13,12 @@ export default function PageLoadingPlaceholder() {
     return () => dispatch(unload());
   }, [dispatch]);
 
-  return <p className="lead">Chargement de la page en cours&hellip;</p>;
+  return (
+    <>
+      <p className="lead text-center">Chargement de la page en cours&hellip;</p>
+      <div className="text-center">
+        <FontAwesomeIcon icon={faCircleNotch} spin size="3x" />
+      </div>
+    </>
+  );
 }
