74a7c11565e3b437089198c9b137f1b66ba223ef
Pierre Gentile <pierre.gentile.perso@gmail.com>
2021-12-04 12:45:06 +0100

    Babel config cleanup

--- zucchini-ui-frontend/babel.config.js
+++ zucchini-ui-frontend/babel.config.js
@@ -23,9 +23,6 @@ module.exports = (api) => {
 
   return {
     plugins: [
-      "@babel/plugin-proposal-class-properties",
-      "@babel/plugin-proposal-nullish-coalescing-operator",
-      "@babel/plugin-proposal-optional-chaining",
       "babel-plugin-lodash",
       [
         "@babel/plugin-transform-runtime",
