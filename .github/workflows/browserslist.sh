240d1b556645c61eeba26abc8a0175d16cacc699
Pierre Gentile <pierre.gentile.perso@gmail.com>
2021-12-19 17:02:52 +0100

    Fix script

--- .github/workflows/browserslist.sh
+++ .github/workflows/browserslist.sh
@@ -3,10 +3,14 @@
 set -e
 set -o pipefail
 
+git branch -C browserslist
+git checkout browserslist
+
+(cd zucchini-ui-frontend && npx browserslist@latest --update-db)
+(cd zucchini-ui-e2e-tests && npx browserslist@latest --update-db)
+
 count_changes=$(git status --porcelain | wc -l)
 if [[ $count_changes -gt 0 ]]; then
-  git branch -C browserslist
-  git checkout browserslist
 
   git add -u
   gh pr view || gh pr create --title "Update browserslist database" --label browserslist --body "Automatic upgrade of the database"
