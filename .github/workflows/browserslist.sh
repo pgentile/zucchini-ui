6bbab3de5955cc918e813917499372f6811c3d54
Pierre Gentile <pierre.gentile.perso@gmail.com>
2021-12-19 16:42:44 +0100

    Authenticate to Github

--- .github/workflows/browserslist.sh
+++ .github/workflows/browserslist.sh
@@ -9,6 +9,7 @@ if [[ $count_changes -gt 0 ]]; then
   git checkout browserslist
 
   git add -u
+  echo "$GITHUB_TOKEN" | gh auth login --with-token
   gh pr view || gh pr create --title "Update browserslist database" --label browserslist --body "Automatic upgrade of the database"
   git commit -m "Upgrade the browserslist database"
   git push --force
