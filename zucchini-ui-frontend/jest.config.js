a27210a690d3954c1cef14d37c325d8a21d40e99
Pierre Gentile <pierre.gentile.perso@gmail.com>
2022-08-18 08:33:22 +0200

    Add jsdom test environment

--- zucchini-ui-frontend/jest.config.js
+++ zucchini-ui-frontend/jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   roots: ["./src"],
   transformIgnorePatterns: ["/node_modules/(?!lodash-es)/"],
   coverageDirectory: "../build/coverage",
-  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"]
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+  testEnvironment: "jsdom"
 };
