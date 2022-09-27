f3a11ccc4ae5764050c518a42690235bfac871d1
Pierre Gentile <pgentile@users.noreply.github.com>
2022-09-27 22:28:39 +0200

    Cypress - Add retries

--- zucchini-ui-e2e-tests/cypress.config.ts
+++ zucchini-ui-e2e-tests/cypress.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   defaultCommandTimeout: 8000,
   requestTimeout: 10000,
+  retries: {
+    runMode: 2
+  },
   e2e: {
     baseUrl: "http://localhost:8080/"
   }
