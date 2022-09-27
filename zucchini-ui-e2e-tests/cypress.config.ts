bb5afbe8d0051aa32689be500fed95bde62c15a5
Pierre Gentile <pgentile@users.noreply.github.com>
2022-09-27 22:22:27 +0200

    Cypress - Longer timeouts

--- zucchini-ui-e2e-tests/cypress.config.ts
+++ zucchini-ui-e2e-tests/cypress.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  defaultCommandTimeout: 8000,
+  requestTimeout: 10000,
   e2e: {
     baseUrl: "http://localhost:8080/"
   }
