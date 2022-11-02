53b25a206d546776195cb06e07c6808f07176919
Pierre Gentile <pierre.gentile.perso@gmail.com>
2022-11-02 19:40:53 +0100

    Jest: add JUnit XML test report

--- zucchini-ui-frontend/jest.config.js
+++ zucchini-ui-frontend/jest.config.js
@@ -1,7 +1,19 @@
+const path = require("path");
+
 module.exports = {
   roots: ["./src"],
   transformIgnorePatterns: ["/node_modules/(?!lodash-es)/"],
   coverageDirectory: "../build/coverage",
   setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
-  testEnvironment: "jsdom"
+  testEnvironment: "jsdom",
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      {
+        outputDirectory: path.join(__dirname, "test-results"),
+        outputName: "TEST-jest.xml"
+      }
+    ]
+  ]
 };
