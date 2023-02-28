3b7c7b6b2085cbb40c9ffb2f50a10a89855e3fd5
Pierre Gentile <pierre.gentile.perso@gmail.com>
2023-02-28 21:52:46 +0100

    Publish images to the Github Container Registry

--- buildSrc/src/main/groovy/io/zucchini/build/docker/DockerExtension.groovy
+++ buildSrc/src/main/groovy/io/zucchini/build/docker/DockerExtension.groovy
@@ -10,26 +10,26 @@ class DockerExtension {
 
     String name
 
-    String repository
+    @Input
+    @Optional
+    List<String> repositories = []
 
     @Input
     @Optional
     List<String> tags = []
 
     DockerExtension(Project project) {
-        repository = project.properties['docker.baseRepository']
         name = project.name
         tags << (project.version as String)
     }
 
-    void tag(String tag) {
-        tags << tag
-    }
-
     @PackageScope
     List<String> getFullTags() {
-        String fullName = repository == null ? name : "${repository}/${name}"
-        return tags.collect { "${fullName}:${it}" }
+        return repositories
+            .collect { repository ->
+                tags.collect { tag -> "$repository/${name}:$tag" }
+            }
+            .flatten()
     }
 
 }
