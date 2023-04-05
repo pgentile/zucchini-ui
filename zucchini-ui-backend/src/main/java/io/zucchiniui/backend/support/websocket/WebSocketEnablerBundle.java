ef1fcff4ff184811a3e6a6e7b93b4c3ee94712e5
Pierre Gentile <pierre.gentile.perso@gmail.com>
2023-04-05 20:51:36 +0200

    Fix application loading

--- zucchini-ui-backend/src/main/java/io/zucchiniui/backend/support/websocket/WebSocketEnablerBundle.java
+++ zucchini-ui-backend/src/main/java/io/zucchiniui/backend/support/websocket/WebSocketEnablerBundle.java
@@ -5,15 +5,8 @@ import io.dropwizard.core.ConfiguredBundle;
 import io.dropwizard.core.setup.Bootstrap;
 import io.dropwizard.core.setup.Environment;
 import org.eclipse.jetty.websocket.jakarta.server.config.JakartaWebSocketServletContainerInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import jakarta.servlet.ServletContextEvent;
-import jakarta.servlet.ServletContextListener;
 
 public class WebSocketEnablerBundle implements ConfiguredBundle<Configuration> {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(WebSocketEnablerBundle.class);
 
     @Override
     public void initialize(final Bootstrap<?> bootstrap) {
@@ -22,21 +15,7 @@ public class WebSocketEnablerBundle implements ConfiguredBundle<Configuration> {
 
     @Override
     public void run(final Configuration config, final Environment environment) {
-        environment.servlets().addServletListeners(new ServletContextListener() {
-
-            @Override
-            public void contextInitialized(final ServletContextEvent servletContextEvent) {
-                LOGGER.info("Configuring WebSocket container");
-
-                JakartaWebSocketServletContainerInitializer.configure(environment.getApplicationContext(), null);
-            }
-
-            @Override
-            public void contextDestroyed(final ServletContextEvent servletContextEvent) {
-                // Nothing to do
-            }
-
-        });
+        JakartaWebSocketServletContainerInitializer.configure(environment.getApplicationContext(), null);
     }
 
 }
